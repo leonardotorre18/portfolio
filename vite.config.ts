e1f5650f5f75beefd51a76723781ef0221fe1c37
Leonardotorre18 <Leonardotorre14@gmail.com>
2024-09-12 13:45:06 -0400

    sitemap plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [react({ 
     fastRefresh: false 
-  })],
+  }), Sitemap({ hostname: 'https://leonardotorre.netlify.app' })],
   test:{
     globals: true,
     fastRefresh: false,
