052d5f1dbb562f09794c60c32440609a899da2ac
Leonardotorre18 <Leonardotorre14@gmail.com>
2025-05-18 20:03:17 -0400

    feat: update Footer component to use dynamic GitHub image URL and add GITHUB_URL to environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly WHATSAPP_URL: string;
   readonly INSTAGRAM_URL: string;
   readonly LINKEDIN_URL: string;
+  readonly GITHUB_URL: string;
   // more env variables...
 }
 
